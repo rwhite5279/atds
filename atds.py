3fe0047dbfe75887cb4da0a558d4e93fe0b33f96
Richard White <rwhite@crashwhite.com>
2024-03-01 08:45:20 -0800

    Implemented UnorderedList class, passes all tests.

--- atds.py
+++ atds.py
@@ -90,6 +90,171 @@ class Deque(object):
     def is_empty(self):
         return len(self.deque) == 0
 
+class Node(object):
+    """Defines a node class to be used in an
+    UnorderedList, coming soon.
+    """
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    def set_data(self, data):
+        self.data = data
+    def get_data(self):
+        return self.data
+    def set_next(self, next):
+        self.next = next
+    def get_next(self):
+        return self.next
+    def __repr__(self):
+        return "Node[data=" + str(self.data) + ",next=" + str(self.next) + "]"
+
+class UnorderedList(object):
+    """Defines an unordered (unsorted) list of nodes.
+    """
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+
+    def add(self, data):
+        """Inserts a new node at the beginning of
+        the list
+        """
+        temp_node = Node(data)
+        temp_node.set_next(self.head)
+        self.head = temp_node
+
+    def length(self):
+        """Identifies the length of the list by
+        going through the entire list. Painful!
+        """
+        node_count = 0
+        current = self.head
+        while current != None:
+            current = current.get_next()
+            node_count += 1
+        return node_count 
+
+    def search(self, data):
+        """Returns True if the data is on the list.
+        """
+        current = self.head
+        found = False
+        while current != None and not found: 
+            if current.get_data() == data:
+                found = True
+            else:
+                current = current.get_next()
+        return found
+
+    def remove(self, data):
+        """Removes multiple occurrences of data on the list, 
+        which requires going through the entire list until
+        you hit the end, or nothing if the data isn't on the list.
+        """
+        current = self.head
+        previous = None
+        while current != None and self.head != None:    # Have to search entire list
+            if current.get_data() == data:              # need to remove it
+                if previous == None:                    # we're at the head
+                    self.head = current.get_next()
+                    current = current.get_next()
+                else:
+                    previous.set_next(current.get_next())
+                    current = current.get_next()
+            else:                                       # pass on through
+                previous = current
+                current = current.get_next()
+
+    def append(self, data):
+        """Appends an item to the end of the list
+        """
+        temp = Node(data) 
+        current = self.head
+        while current.get_next() != None:
+            current = current.get_next()
+        current.set_next(temp)
+
+    def index(self, data):
+        """Returns the index of the first occurence of the data
+        in the list.
+        """
+        if self.head == None:
+            return None
+        current = self.head
+        index = 0
+        while current != None and current.get_data() != data:
+            current = current.get_next()
+            index += 1
+        if current == None:
+            return None
+        else:
+            return index
+
+
+
+    def insert(self, position, data):
+        """Inserts the piece of data at the indicated position.
+        """
+        temp = Node(data)
+        index = 0
+        current = self.head
+        previous = None
+        while index < position:
+            previous = current
+            current = current.get_next()
+            index += 1
+        if index == 0:
+            temp.set_next(current)
+            self.head = temp
+        else:
+            previous.set_next(temp)
+            temp.set_next(current)
+            
+    def pop(self, index=-1):
+        """Removes item at position index, or at the end of the list
+        (-1) if no index is indicated.
+        """
+        if self.head == None:
+            return None      # Can't pop from empty list
+        if index == -1:
+            current = self.head
+            previous = None
+            while current.get_next() != None:
+                previous = current
+                current = current.get_next()
+            result = current.get_data()
+            previous.set_next(None)
+            return result
+        elif index == 0:
+            current = self.head
+            result = current.get_data()
+            self.head = current.get_next()
+            return result
+        else:       # returning from middle of list?
+            current = self.head
+            previous = None
+            position = 0
+            while position < index:
+                previous = current
+                current = current.get_next()
+                position += 1
+            result = current.get_data()
+            previous.set_next(current.get_next())
+            return result
+
+    def __repr__(self):
+        """Creates a representation of the list suitable for printing, debugging.
+        """
+        result = "UnorderedList["
+        next_node = self.head
+        while next_node != None:
+            result += str(next_node.get_data()) + ","
+            next_node = next_node.get_next()
+        result = result + "]"
+        return result
+
 def main():
     pass
 
